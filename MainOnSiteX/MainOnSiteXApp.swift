99cca47fb9ecb673fc76f8153bd52f49a8be5ef6
Tristan Shelton <trishelton@corelogic.com>
2024-06-18 23:26:16 -0500

    first commit

---
+++ MainOnSiteX/MainOnSiteXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainOnSiteXApp.swift
+//  MainOnSiteX
+//
+//  Created by Victor Ramirez on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct MainOnSiteXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
